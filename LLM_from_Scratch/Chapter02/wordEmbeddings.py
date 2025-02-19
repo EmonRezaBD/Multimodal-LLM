58c4ed4d820487cd265b717b6f91ed620d430eb1
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-02-19 13:01:19 +0600

    Tokenizing text

--- LLM_from_Scratch/Chapter02/wordEmbeddings.py
+++ LLM_from_Scratch/Chapter02/wordEmbeddings.py
@@ -1,9 +1,25 @@
-print("Start!")
+import re
 
+print("Start!\n")
+
+# Read the text file : short story for tokenizing : Comment 01
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
-    raw_text = f.read()
+    raw_text = f.read() 
 
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
+# print("Total number of character:", len(raw_text))
+# print(raw_text[:99])
 
-print("End!")
+# Tokenizing the text : From raw text to list of words : Comment 02
+
+# Testtext = "Hello, world. This, is a test."
+# Tokenizetext01 = re.split(r'(\s)',Testtext)
+# print(Tokenizetext01)
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+print(preprocessed[:30])
+
+
+
+print("\nEnd!\n")
