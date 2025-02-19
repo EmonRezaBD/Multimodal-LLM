cdd6b04f0d30a7d7024cf584327ce340116ac163
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-02-19 12:36:35 +0600

    Start LLM building
    Now doing word embeddings

---
+++ LLM_from_Scratch/Chapter02/wordEmbeddings.py
@@ -0,0 +1,9 @@
+print("Start!")
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+print("Total number of character:", len(raw_text))
+print(raw_text[:99])
+
+print("End!")
