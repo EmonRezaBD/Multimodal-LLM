f724b1a91ef20966bf4be10ea2ad7940c320a7d6
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-02-19 17:50:49 +0600

    Token and token IDS
    Encode and Decode texts

--- LLM_from_Scratch/Chapter02/wordEmbeddings.py
+++ LLM_from_Scratch/Chapter02/wordEmbeddings.py
@@ -1,6 +1,24 @@
 import re
+import os
+os.chdir("F:\GithubRepo\Multimodal-LLM\LLM_from_Scratch\Chapter02") # set the directory to debug the code
 
 print("Start!\n")
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
 
 # Read the text file : short story for tokenizing : Comment 01
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
@@ -17,8 +35,35 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
 
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
-print(len(preprocessed))
-print(preprocessed[:30])
+# # print(len(preprocessed))
+# # print(preprocessed[:30])
+
+# #Converting tokens into token IDs : Comment 03
+
+all_words = sorted(list(set(preprocessed))) #unique tokens
+vocab_size = len(all_words)
+# print(vocab_size)
+
+vocab = {token:integer for integer, token in enumerate(all_words)} #unique tokens IDs
+# # for i, item in enumerate(vocab.items()):
+#     if i < 10:
+#         print(item)
+#         if(i>50):
+#             break
+
+
+#Using class
+tokenizer = SimpleTokenizerV1(vocab)
+
+text = """"I It's the last he painted, you know," 
+           Mrs. Gisburn said with pardonable pride.""" #old text
+
+testText = "Hello, Any or Poison?" #new text : it comes error
+ids = tokenizer.encode(testText)
+print(ids)
+print(tokenizer.decode(ids))
+
+
 
 
 
